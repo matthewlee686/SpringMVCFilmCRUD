f13a39fec1c48aa03b6f41342a2dfe4c33dc73d3
ren09 <rachel.richardson915@gmail.com>
2021-07-03 12:11:36 -0600

    pushing film DAO and accessor

---
+++ src/main/java/com/skilldistillery/film/dao/FilmAccessor.java
@@ -0,0 +1,19 @@
+package com.skilldistillery.film.dao;
+
+import java.sql.SQLException;
+import java.util.List;
+
+import com.skilldistillery.film.entities.Actor;
+import com.skilldistillery.film.entities.Film;
+
+public interface FilmAccessor {
+	public Film findFilmById(int filmId) throws SQLException;
+	  public Actor findActorById(int actorId) throws SQLException;
+	  public List<Actor> findActorsByFilmId(int filmId);
+	  public List<Film> findFilmByKeyword(String keyword) throws SQLException;
+	  public Film createFilm(Film film) throws SQLException;
+	  public void deleteFilmByID(int filmID) throws SQLException;
+	  public void updateFilmByID(int filmID, String title, String desc, String releaseYear, int langID, 
+				int rentDur, double rentRate, int length, double repCost) throws SQLException;
+
+}
