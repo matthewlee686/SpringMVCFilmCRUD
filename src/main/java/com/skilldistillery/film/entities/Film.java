417c2463111c4c4b2ce4d828a8087678a3dd7bc2
ren09 <rachel.richardson915@gmail.com>
2021-07-03 12:21:01 -0600

    pushing Actor and Film

--- src/main/java/com/skilldistillery/film/entities/Film.java
+++ src/main/java/com/skilldistillery/film/entities/Film.java
@@ -1,5 +1,221 @@
 package com.skilldistillery.film.entities;
 
+import java.util.List;
+
+import com.skilldistillery.film.entities.Actor;
+
 public class Film {
+	private int id;
+	private String title;
+	private String desc;
+	private String releaseYear;
+	private String language;
+	private int rentalDuration;
+	private double rentalRate;
+	private String length;
+	private double replaceCost;
+	private String rating;
+	private String specialFeatures;
+	private List<Actor> actors;
+	
+	
+	public Film() {
+	
+	}
+
+	public Film(int id, String title, String desc, String releaseYear, String language, int rentalDuration,
+			double rentalRate, String length, double replaceCost, String rating, String specialFeatures, List<Actor> actors) {
+		super();
+		this.id = id;
+		this.title = title;
+		this.desc = desc;
+		this.releaseYear = releaseYear;
+		this.language = language;
+		this.rentalDuration = rentalDuration;
+		this.rentalRate = rentalRate;
+		this.length = length;
+		this.replaceCost = replaceCost;
+		this.rating = rating;
+		this.specialFeatures = specialFeatures;
+		this.actors = actors;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((actors == null) ? 0 : actors.hashCode());
+		result = prime * result + ((desc == null) ? 0 : desc.hashCode());
+		result = prime * result + id;
+		result = prime * result + ((language == null) ? 0 : language.hashCode());
+		result = prime * result + ((length == null) ? 0 : length.hashCode());
+		result = prime * result + ((rating == null) ? 0 : rating.hashCode());
+		result = prime * result + ((releaseYear == null) ? 0 : releaseYear.hashCode());
+		result = prime * result + rentalDuration;
+		long temp;
+		temp = Double.doubleToLongBits(rentalRate);
+		result = prime * result + (int) (temp ^ (temp >>> 32));
+		temp = Double.doubleToLongBits(replaceCost);
+		result = prime * result + (int) (temp ^ (temp >>> 32));
+		result = prime * result + ((specialFeatures == null) ? 0 : specialFeatures.hashCode());
+		result = prime * result + ((title == null) ? 0 : title.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Film other = (Film) obj;
+		if (actors == null) {
+			if (other.actors != null)
+				return false;
+		} else if (!actors.equals(other.actors))
+			return false;
+		if (desc == null) {
+			if (other.desc != null)
+				return false;
+		} else if (!desc.equals(other.desc))
+			return false;
+		if (id != other.id)
+			return false;
+		if (language == null) {
+			if (other.language != null)
+				return false;
+		} else if (!language.equals(other.language))
+			return false;
+		if (length == null) {
+			if (other.length != null)
+				return false;
+		} else if (!length.equals(other.length))
+			return false;
+		if (rating == null) {
+			if (other.rating != null)
+				return false;
+		} else if (!rating.equals(other.rating))
+			return false;
+		if (releaseYear == null) {
+			if (other.releaseYear != null)
+				return false;
+		} else if (!releaseYear.equals(other.releaseYear))
+			return false;
+		if (rentalDuration != other.rentalDuration)
+			return false;
+		if (Double.doubleToLongBits(rentalRate) != Double.doubleToLongBits(other.rentalRate))
+			return false;
+		if (Double.doubleToLongBits(replaceCost) != Double.doubleToLongBits(other.replaceCost))
+			return false;
+		if (specialFeatures == null) {
+			if (other.specialFeatures != null)
+				return false;
+		} else if (!specialFeatures.equals(other.specialFeatures))
+			return false;
+		if (title == null) {
+			if (other.title != null)
+				return false;
+		} else if (!title.equals(other.title))
+			return false;
+		return true;
+	}
+
+	public String toString() {
+		return  "\nFilm:\n"
+				+ "----\n" 
+				+ "ID: " + id + "\n" 
+				+ "Title: " + title +" " + "\n" 
+				+ "Release Year: " + releaseYear + "\n" 
+				+ "Rating: " + rating + "\n"
+				+ "Description: " + desc +" " + "\n" 
+				+ "Language: " + language + "\n"
+				+ "=============================================\n"
+				+ "Starring:"
+				+ "\n---------\n"
+				+ actors
+//				+ "\nThere are: " + actors.size() + " Actor(s) in this Film\n"
+				+ "=============================================\n";
+	}
+	
+	public String getLanguage() {
+		return language;
+	}
+
+	public void setLanguage(String language) {
+		this.language = language;
+	}
+
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getDesc() {
+		return desc;
+	}
+	public void setDesc(String desc) {
+		this.desc = desc;
+	}
+	public String getReleaseYear() {
+		return releaseYear;
+	}
+	public void setReleaseYear(String releaseYear) {
+		this.releaseYear = releaseYear;
+	}
+	
+	public int getRentalDuration() {
+		return rentalDuration;
+	}
+	public void setRentalDuration(int rentalDuration) {
+		this.rentalDuration = rentalDuration;
+	}
+	public double getRentalRate() {
+		return rentalRate;
+	}
+	public void setRentalRate(double rentalRate) {
+		this.rentalRate = rentalRate;
+	}
+	public String getLength() {
+		return length;
+	}
+	public void setLength(String length) {
+		this.length = length;
+	}
+	public double getReplaceCost() {
+		return replaceCost;
+	}
+	public void setReplaceCost(double replaceCost) {
+		this.replaceCost = replaceCost;
+	}
+	public String getRating() {
+		return rating;
+	}
+	public void setRating(String rating) {
+		this.rating = rating;
+	}
+	public String getSpecialFeatures() {
+		return specialFeatures;
+	}
+	public void setSpecialFeatures(String specialFeatures) {
+		this.specialFeatures = specialFeatures;
+	}
+
+	public List<Actor> getActors() {
+		return actors;
+	}
+
+	public void setActors(List<Actor> actors) {
+		this.actors = actors;
+	}	
+	
 
 }
